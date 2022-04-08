f3479a03ae8b484ea9e7b1fc4e2decaa0cb767e7
poldiee <nessidave@gmail.com>
2022-04-08 05:05:27 +0300

    all chanes

---
+++ Documents/ANGULAR/Goals/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Goal } from './goal';
+// ....
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  // // goal = 'Watch Finding Nemo'
+  title = 'My Goals';
+  // goals:string[];
+
+  // constructor(){
+  //   this.goals = ['Watch finding Nemo','Buy cookies', 'Get new phone case']
+
+}
+
